dac42bc320bc4b7f37f0573d952eb2837e1b1c8b
zakbyeol <macaron010@naver.com>
2025-11-30 06:30:30 +0000

    BOJ1918_Answer

--- Baekjoon/Gold_Tier/BOJ1918_G2/Correct.c
+++ Baekjoon/Gold_Tier/BOJ1918_G2/Correct.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum
+{
+    LEFT_PARENTHESIS = '(', RIGHT_PARENTHESIS = ')',
+    PLUS = '+', MINUS = '-',
+    MULTIPLY = '*', DIVIDE = '/',
+}OPERATOR;
+
 // 전역 변수 대신 메인 함수 내에서 스택 관련 변수 관리 권장
 
 // 연산자 우선순위 반환 함수
-// 괄호는 스택 내부에서 가장 낮은 우선순위로 간주하여, 자기보다 낮은 연산자가 올 때까지 팝을 막습니다.
+// 괄호는 스택 내부에서 가장 낮은 우선순위로 간주하여, 자기보다 낮은 연산자가 올 때까지 팝을 막는다.
 int GetPriority(char OPERATOR)
 {
     switch (OPERATOR)
     {
-    case '*':
-    case '/':
+    case MULTIPLY:
+    case DIVIDE:
         return 2; // 곱셈/나눗셈: 2
-    case '+':
-    case '-':
+    case PLUS:
+    case MINUS:
         return 1; // 덧셈/뺄셈: 1
-    case '(':
+    case LEFT_PARENTHESIS:
         return 0; // 스택 내부에서 '('는 가장 낮은 우선순위 (0)
     default:
         return -1; // 잘못된 연산자
     }
 }
 
-// 스택의 푸시 연산을 수행합니다.
+// 스택의 푸시 연산
 void Push(char* stack, int* top, char data)
 {
     stack[(*top)++] = data;
 }
 
-// 스택의 팝 연산을 수행하고 그 결과를 반환합니다.
+// 스택의 팝 연산을 수행하고 그 결과를 반환한다.
 char Pop(int* top, char* stack)
 {
     if (*top == 0) return '\0'; // 스택이 비어있으면 널 문자 반환
@@ -38,16 +45,12 @@ char Pop(int* top, char* stack)
 
 int main()
 {
-    // 입력 중위 표기식 (최대 100자)
     char InfixNotation[101]; 
-    // 출력 후위 표기식 (최대 입력 길이와 같음)
     char PostfixNotation[101];
     int PostfixIndex = 0;
 
-    // 연산자 스택
     char OperatorStack[101];
-    // top: 스택의 다음 푸시 위치 (요소의 개수)
-    int StackTop = 0; 
+    int StackTop = 0; // 스택의 요소의 갯수
 
     // 입력 받기
     scanf("%s", InfixNotation);
@@ -63,21 +66,21 @@ int main()
             // 1. 피연산자 (문자)일 때: 그대로 출력 문자열에 추가
             PostfixNotation[PostfixIndex++] = Token;
         }
-        else if (Token == '(')
+        else if (Token == LEFT_PARENTHESIS)
         {
             // 2. 왼쪽 괄호일 때: 스택에 푸시
             Push(OperatorStack, &StackTop, Token);
         }
-        else if (Token == ')')
+        else if (Token == RIGHT_PARENTHESIS)
         {
             // 3. 오른쪽 괄호일 때: 
             // 스택에서 '('가 나올 때까지 Pop하여 출력
-            while (StackTop > 0 && OperatorStack[StackTop - 1] != '(')
+            while (StackTop > 0 && OperatorStack[StackTop - 1] != LEFT_PARENTHESIS)
             {
                 PostfixNotation[PostfixIndex++] = Pop(&StackTop, OperatorStack);
             }
             // '('를 스택에서 제거 (팝)하고 출력하지 않음
-            if (StackTop > 0 && OperatorStack[StackTop - 1] == '(')
+            if (StackTop > 0 && OperatorStack[StackTop - 1] == LEFT_PARENTHESIS)
             {
                 Pop(&StackTop, OperatorStack);
             }
@@ -101,13 +104,10 @@ int main()
     {
         PostfixNotation[PostfixIndex++] = Pop(&StackTop, OperatorStack);
     }
-
-    // 결과 문자열의 끝에 널 문자 추가
     PostfixNotation[PostfixIndex] = '\0';
     
     // 결과 출력
     printf("%s\n", PostfixNotation);
 
-    // 동적 할당을 사용하지 않아 free 불필요
     return 0;
 }
